b3570445c4b4d2f79003c7a352c160f5c1879cb3
Fernando <contact@fernhw.com>
2020-07-11 11:38:07 -0500

    added more inputs

--- Assets/Game/Script/Models/InputsManager.cs
+++ Assets/Game/Script/Models/InputsManager.cs
@@ -2,11 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// All the input in the game.
+/// </summary>
+
 public class InputsManager
 {
 
+    //
+    // Any button is true for Exclusively one frame
+    //
+
+
+    /// <summary>
+    /// Axis X,Y
+    /// </summary>
     public Vector2 movement;
+
+  
+    /// <summary>
+    /// Axis X,Y
+    /// </summary>
     public bool actionButton;
+
+    /// <summary>
+    /// A button makes the penguin make a sound
+    /// </summary>
+    public bool quak;
+
+    /// <summary>
+    /// X,Y towards the screen
+    /// </summary>
     public Vector2 pointer;
+
+    /// <summary>
+    /// Click the screen
+    /// </summary>
+    public bool click;
+
 
 }
