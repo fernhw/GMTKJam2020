70ff251801c053cce5ae114a157d35ba225ff630
Fernando <contact@fernhw.com>
2020-07-11 11:06:43 -0500

    penguin feet fixed, + small architecture adjustments

--- Assets/Game/Script/MonoBehaviours/InputManager.cs
+++ Assets/Game/Script/MonoBehaviours/InputManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectManager : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
