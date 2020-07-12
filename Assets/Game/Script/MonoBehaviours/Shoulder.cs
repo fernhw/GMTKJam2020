e60e7e3a29200f3ab7301cea51ba920fe5f07e89
Steve Seung <qedseung@gmail.com>
2020-07-11 19:01:00 -0700

    walking animation

---
+++ Assets/Game/Script/MonoBehaviours/Shoulder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoulder : MonoBehaviour
+{
+    [SerializeField] GameObject ArmSprite;
+    [SerializeField] float angle = 0;
+
+    Vector3 pivot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pivot = transform.forward;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ArmSprite.transform.RotateAround(transform.position, pivot, angle * Time.deltaTime);   
+    }
+
+    public void GameUpdate(Deltas delta, InputsManager input)
+    {
+        //GameRunner shall call this to rotate arm to angle         
+    }
+}
