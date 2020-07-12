e2b0b85082a3fb043e1a4f83c63e4f93a948d0ea
Fernando <contact@fernhw.com>
2020-07-12 00:00:21 -0500

    bullets are in

--- Assets/Game/Script/MonoBehaviours/Settings.cs
+++ Assets/Game/Script/MonoBehaviours/Settings.cs
@@ -9,8 +9,12 @@ public class Settings : MonoBehaviour
 
     public float characterSpeed = 20;
 
+    public float bulletSpeed = 10;
+
     public float penguinYUpPivotFix = 0.6f;
 
     public Vector2 cameraLerpSpeed = new Vector2(0,0);
+
+    public Vector2 cameraBounds = new Vector2(8, 6);
 
 }
--- Assets/Game/Script/Systems/ShootingAction.cs
+++ Assets/Game/Script/Systems/ShootingAction.cs
@@ -8,27 +8,65 @@ using UnityEngine;
 public class ShootingAction {
 
      public static void Parse (ref Data data, ref World world, Deltas delta, InputsManager controls, Settings settings) {
-
+        Vector3 cameraPosition = world.cam.transform.localPosition;
         Vector3 characterPos = world.penguinObj.transform.localPosition;
+
+        float cameraUpperBound = cameraPosition.y + settings.cameraBounds.y;
+        float cameraLowerBound = cameraPosition.y - settings.cameraBounds.y;
+        float cameraRightBound = cameraPosition.x + settings.cameraBounds.x;
+        float cameraLeftBound = cameraPosition.x - settings.cameraBounds.x;
+
         characterPos.y+= settings.penguinYUpPivotFix;
 
         int bulletPoolLen = world.bulletPool.Count;
-        Bullet currentBullet;
+
+        int activeBulletIndex = 0;
 
         // pick innactive bullet
         for (int i = 0; i< bulletPoolLen; i++){
-            Bullet indexBullet;
+            Bullet indexBullet = world.bulletPool[i];
             if(!indexBullet.active){
-                currentBullet = indexBullet;
+                activeBulletIndex = i;
                 break;
             }
         }
 
-        // animate bullets
-            
 
-        if(controls.actionButton){
-            world.bulletPool[0].transform.localPosition = characterPos;
+        // animate bullets
+        if (controls.actionButton){
+            world.bulletPool[activeBulletIndex].gameObject.SetActive(true);
+            world.bulletPool[activeBulletIndex].active = true;
+            world.bulletPool[activeBulletIndex].transform.localPosition = characterPos;
+            world.bulletPool[activeBulletIndex].transform.localEulerAngles = new Vector3(0, 0, data.angleToMouse);
+        }
+
+
+        // animate bullets
+        for (int i = 0; i < bulletPoolLen; i++) {
+
+            Bullet movingBullet = world.bulletPool[i];
+
+            if (!movingBullet.active)
+                continue;
+
+            Vector3 bulletPosition = movingBullet.transform.localPosition;
+            Vector3 bulletAngle = movingBullet.transform.localEulerAngles;
+
+            Vector3 bulletMovement = new Vector3(Mathf.Cos(bulletAngle.z*Mathf.Deg2Rad), Mathf.Sin(bulletAngle.z * Mathf.Deg2Rad), 0); ;
+
+            Vector3 newBulletPosition = bulletPosition + bulletMovement * delta.actionDelta * settings.bulletSpeed;
+
+            movingBullet.transform.localPosition = newBulletPosition;
+            bulletPosition = movingBullet.transform.localPosition;
+
+            if (bulletPosition.x > cameraRightBound ||
+                bulletPosition.x < cameraLeftBound ||
+                bulletPosition.y > cameraUpperBound ||
+                bulletPosition.y < cameraLowerBound) {
+                movingBullet.gameObject.SetActive(false);
+                movingBullet.active = false;
+            }
+            //world.bulletPool[0].transform.localPosition = characterPos;
         }
 
     }
